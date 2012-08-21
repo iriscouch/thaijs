5ec46906aa0d23ea35edff2f20a34bd22e318945
Jason Smith (work) <jhs@iriscouch.com>
2012-08-21 05:30:38 +0000

    Remove old push code

--- push.sh
+++ push.sh
@@ -28,34 +28,3 @@ while true; do
   echo "Restart"
   sleep 1
 done
-
-var fs = require('fs')
-var SP = require('static-plus')
-
-if(require.main === module)
-  main()
-
-function main() {
-  var DB = process.argv[2] || process.env.db
-  if(!DB)
-    return console.error('Usage: build.js <database_url>')
-
-  var builder = new SP.Builder
-  builder.target = DB + '/thaijs'
-  builder.read_only = true
-
-  // The landing page and (generated) Javascript is static attachments.
-  var land = [ '<html>'
-             ,   '<body>'
-             ,     '<p>Thai JS</p>'
-             ,   '</body>'
-             , '</html>'
-             ].join('')
-
-  builder.page('', land, 'text/html')
-
-  builder.deploy()
-  builder.on('deploy', function(result) {
-    console.log('Deployed: %s/%s/', result.url, builder.namespace)
-  })
-}
