744845625b710eec7cc4b648dbe25e8eacedb4d3
Jason Smith (work) <jhs@iriscouch.com>
2012-08-21 05:30:52 +0000

    The naked domain should be staging during development

--- push.sh
+++ push.sh
@@ -10,18 +10,22 @@ fi
 if [ "$1" = "dev" ]; then
   couch="http://localhost:5984"
   domain="two.local"
+  pro="www."
+  staging=""
 elif [ -z "$pw" ]; then
   read -s -p "CouchDB password: " pw
   echo
 
   couch="https://jhs:$pw@jhs.iriscouch.com"
   domain="thaijs.com"
+  pro=""
+  staging="staging."
 fi
 
 while true; do
   static+ "$couch" thaijs "$domain"                             \
           --log=debug                                           \
-          --prefix='' --staging-prefix='st.'                    \
+          --prefix="$pro" --staging-prefix="$staging"           \
           --seed="$PWD/seed" --publish="$PWD/publish"           \
           --watch
 
